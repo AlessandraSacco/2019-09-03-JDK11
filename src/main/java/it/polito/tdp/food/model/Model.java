a83c85fc4223d1cb16bd7e66a0f8b3640dbbef11
aless <aless@LAPTOP-M1QLFVH8.fritz.box>
2022-06-15 11:31:16 +0200

    prova1

--- src/main/java/it/polito/tdp/food/model/Model.java
+++ src/main/java/it/polito/tdp/food/model/Model.java
@@ -16,73 +16,7 @@ import it.polito.tdp.food.db.FoodDao;
 
 public class Model {
 	
-	SimpleWeightedGraph<Portion,DefaultWeightedEdge> grafo;
-	FoodDao dao;
-	Map<String,Portion> idMap;
-	List<Arco> archi = new ArrayList<Arco>();
-	
-	public Model() {
-	dao = new FoodDao();
-	idMap= new HashMap<>();
-	dao.listAllPortions(idMap);
-	}
-	
-	public void creaGrafo(Double calories) {
-		grafo= new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
-		
-		// creare i vertici del grafo 
-		Graphs.addAllVertices(this.grafo, dao.getVertici(idMap, calories));
-		
-		// creare gli archi del grafo 
-		for(Arco a: dao.getArchi(idMap,calories)) {
-			if(grafo.containsVertex(a.getP1()) && grafo.containsVertex(a.getP2())) {
-				if(a.getPeso()>0) {
-			  Graphs.addEdgeWithVertices(this.grafo, a.getP1(), a.getP2(),a.getPeso());		
-				archi.add(a);
-				}
-			}
-		}
-	}
-	
-	public int getVertici() {
-		return grafo.vertexSet().size();
-	}
-	
-	public int getArchi() {
-		return grafo.edgeSet().size();
-	}
-	
-	public List<Portion> listaPorzioni(){
-		List<Portion> porzioni = new ArrayList<Portion>();
-		
-		for(Portion p: this.grafo.vertexSet()) {
-			porzioni.add(p);
-		}
-		
-		return porzioni;
-	}
-	
-	public Graph<Portion,DefaultWeightedEdge> getGrafo(){
-		return grafo;
-	}
-	
-	public List<Portion> getPorzioniCollegate(Portion p){
-		ConnectivityInspector<Portion, DefaultWeightedEdge> ci = new ConnectivityInspector<Portion, DefaultWeightedEdge>(grafo);
-		
-		List<Portion> porzioniCollegate= new ArrayList<Portion>(ci.connectedSetOf(p));
-	    porzioniCollegate.remove(p);
-	    
-	    for(Portion p1: porzioniCollegate) {
-	    for(Arco a: archi) {
-	    	if((a.getP1().equals(p) && a.getP2().equals(p1)) || (a.getP2().equals(p) && a.getP1().equals(p1))){
-	    		p1.setPeso(a.getPeso());
-	    	}
-	    }
-	    }
-	    return porzioniCollegate;
 
-	}
-	
 	
 	
 }
